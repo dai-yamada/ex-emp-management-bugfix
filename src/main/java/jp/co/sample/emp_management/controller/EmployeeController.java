3e07ad781749173b52537246fd0f995c74e176f3
yamadadai <dai.ymd6@gmail.com>
2020-02-19 16:13:22 +0900

    (4-1)従業員一覧の入社日の日付のフォーマットを変更した。

--- src/main/java/jp/co/sample/emp_management/controller/EmployeeController.java
+++ src/main/java/jp/co/sample/emp_management/controller/EmployeeController.java
@@ -1,8 +1,12 @@
 package jp.co.sample.emp_management.controller;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
